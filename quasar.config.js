5e6e33668fa3970fc55df766c68ed8cb71006b43
waranyoo379 <mju6804101379@mju.ac.th>
2026-09-10 23:40:53 +0700

    Add 'Notify'in quasar.comf.js

--- quasar.config.js
+++ quasar.config.js
@@ -84,7 +84,9 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ],
     },
 
     // animations: 'all', // --- includes all animations
